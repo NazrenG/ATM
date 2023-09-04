0b522c59390fd3c2fb1bf16d5d8063e58de1a670
NazrenG <nezrin.qu@gmail.com>
2023-09-04 15:29:03 +0400

    ATM project

---
+++ AtmClass/Card.cs
@@ -0,0 +1,73 @@
+﻿
+
+namespace Task_cs.AtmClass
+{
+    internal partial  class Card
+    {
+        private string? Bankname { get; set; }
+        private string? Pan;
+        private string? Pin;
+        private string? Cvc;
+        private DateTime ExpireDate { get; set; }
+        private decimal Balans;
+
+        public Card()
+        {
+            Bankname = null;
+            Pan = null;
+            Pin = null;
+            Cvc = null;
+            ExpireDate = DateTime.MinValue;
+            Balans = 0;
+        }
+
+        public Card(string? bankname, string? pan, string? pin, string? cvc, DateTime expireDate, decimal balans)
+        {
+            Bankname = bankname;
+            Pan = pan;
+            Pin = pin;
+            Cvc = cvc;
+            ExpireDate = expireDate;
+            Balans = balans;
+        }
+
+        public string PAN
+        {
+            get { return Pan!; }
+            set { if (value.Length == 16) Pan = value;
+                else  throw new Exception("PAN 16 reqemden ibaret olmalidir!!") ;
+            }
+        }
+
+        public string PIN
+        {
+            get { return Pin!; }
+            set
+            {
+                if (value.Length == 4) Pin = value;
+                else throw new Exception("PIN 4 reqemden ibaret olmalidir!!");
+            }
+        }
+
+        public string CVC
+        {
+            get { return Cvc!; }
+            set
+            {
+                if (value.Length == 3) Cvc = value;
+                else throw new Exception("CVC 3 reqemden ibaret olmalidir!!");
+            }
+        }
+        public decimal BALANS
+        {
+            get { return Balans; }
+            set
+            {
+                if (value>=0) Balans = value;
+                else throw new Exception("Balans 0 AZN-den asagi ola bilmez!!");
+            }
+        }
+
+        
+    }
+}
---
+++ AtmClass/User.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Task_cs.AtmClass
+{
+    internal class User
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public Card? CreditCard { get; set; }
+
+        public User()
+        {
+            Name = null;
+            Surname = null;
+            CreditCard = null;
+        }
+
+        public User(string? name, string? surname, Card? creditCard)
+        {
+            Name = name;
+            Surname = surname;
+            CreditCard = creditCard;
+        }
+        public decimal CardBalance => CreditCard!.BALANS;
+        public string CardPan => CreditCard!.PAN;
+        public string CardPin => CreditCard!.PIN;
+        
+
+       
+    }
+}
